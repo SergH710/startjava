07283b936a001644ea798231f59db08e905e1d14
SergH710 <sergejh77@gmail.com>
2021-01-11 21:28:25 +0700

    test lesson 01

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,8 @@
+public class Variable {   
+    public static void main(String[] args) {
+        byte b1 = 12;
+        short s1 = 2000;
+        int N1 = 1200;
+        System.out.println("b1=" + b1);
+    }
+}
